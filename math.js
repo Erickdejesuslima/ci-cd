e0ab19a45051e612cd3961ab5baa8af1aad23581
Erick Lima <erickdejesus.lima01@gmail.com>
2022-06-09 09:17:51 -0300

    trabalho de devops

---
+++ math.js
@@ -0,0 +1,30 @@
+function soma(a, b) {
+    return a + b
+}
+
+console.log(soma(2,4))
+
+function subtracao(a, b) {
+    return a - b
+}
+
+console.log(soma(4,2))
+
+
+function divisao(a, b) {
+return a / b
+}
+console.log(soma(4,2))
+
+function multiplicacao(a, b) {
+    return a * b
+}
+console.log(soma(4,2))
+
+
+module.exports = {
+    soma,
+    subtracao,
+    divisao,
+    multiplicacao
+};
